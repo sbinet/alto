d6a272e39757beb37a331b1812b303178fa3ce1d
Sebastien Binet <binet@cern.ch>
2013-01-28 14:22:58 +0100

    utils: path_exists

---
+++ altolib/utils.go
@@ -0,0 +1,18 @@
+package altolib
+
+import (
+	"os"
+)
+
+func path_exists(name string) bool {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+// EOF
