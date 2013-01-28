c5d523fbf104c10fe8cdbae1b58e6950fa1c8add
Sebastien Binet <binet@cern.ch>
2013-01-28 18:17:33 +0100

    init: non-dummy impl

--- cmd_init.go
+++ cmd_init.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	// "bytes"
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
-	// "os"
+	"os"
 	// "os/exec"
 	// "path/filepath"
 
@@ -34,16 +34,41 @@ func alto_run_cmd_init(cmd *commander.Command, args []string) {
 	var err error
 	n := "alto-" + cmd.Name()
 
+	box_id := ""
 	switch len(args) {
-	case 0:
-		// ok
+	case 1:
+		box_id = args[0]
 	default:
-		err = fmt.Errorf("%s: does not take any argument\n", n)
+		err = fmt.Errorf("%s: need a box id\n", n)
 		handle_err(err)
 	}
 
-	//quiet := cmd.Flag.Lookup("q").Value.Get().(bool)
+	quiet := cmd.Flag.Lookup("q").Value.Get().(bool)
+	if !quiet {
+		fmt.Printf("%s with box=%s...\n", n, box_id)
+	}
 
+	box, err := g_ctx.GetBox(box_id)
+	handle_err(err)
+
+	const fname = "AltoFile"
+	if path_exists(fname) {
+		err = fmt.Errorf("%s: a file [%s] already exists", n, fname)
+		handle_err(err)
+	}
+
+	f, err := os.Create(fname)
+	handle_err(err)
+	defer f.Close()
+
+	err = json.NewEncoder(f).Encode(&box)
+	handle_err(err)
+	handle_err(f.Sync())
+	handle_err(f.Close())
+
+	if !quiet {
+		fmt.Printf("%s with box=%s... [done]\n", n, box_id)
+	}
 	return
 }
 
